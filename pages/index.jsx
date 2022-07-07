899c9bc5a8680ad766a8b4d49ff6363e975fc525
olabamipe.taiwo <olabamipe.taiwo@mypatricia.co>
2022-07-07 12:31:40 +0100

    dev::general pages fixes

--- pages/index.jsx
+++ pages/index.jsx
@@ -187,8 +187,8 @@ const Home = () => {
       <Popup display={showPopUp} closeModal={() => setShow(false)}>
         <section className="w-full bg-white rounded-xl p-3">
           <section className="flex">
-            <div className="basis-1/2 flex-shrink-0">
-              <div className="flex flex-col pt-24 pl-8 pr-28">
+            <div className="basis-full tab:basis-1/2 flex-shrink-0">
+              <div className="flex flex-col px-7 py-14 tab:pt-24 tab:pl-8 tab:pr-28">
                 <h2 className="text-primary-main font-semibold font-body text-3xl mb-[18px]">
                   Welcome to the future
                 </h2>
@@ -200,7 +200,7 @@ const Home = () => {
                 </button>
               </div>
             </div>
-            <div className="basis-1/2 flex-shrink-0">
+            <div className="hidden tab:basis-1/2 flex-shrink-0">
               <figure className="h-[483px]">
                 <img
                   src="https://static.greenafrica.com/media/1003/green-africa_atr_mountain_final.jpeg?cropmode=percentaje&width=1200"
