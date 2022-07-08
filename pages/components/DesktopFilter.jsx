434cfaeef16a55700d31860ded8ff9ad7330ab88
olabamipe.taiwo <olabamipe.taiwo@mypatricia.co>
2022-07-08 02:29:39 +0100

    dev::general pages fixes

--- pages/components/DesktopFilter.jsx
+++ pages/components/DesktopFilter.jsx
@@ -1,7 +1,11 @@
 /* eslint-disable @next/next/no-img-element */
+import { useState } from "react";
 import PromoIcon from "assets/svgs/promo.svg";
+import CheckInTab from "./tabs/CheckInTab";
 
 const DesktopFilter = () => {
+  const [activeTab, setActiveTab] = useState(3);
+
   return (
     <section className="ga__desktop__filter w-full bg-green min-h-[168px] flex flex-col">
       <div className="ga__desktop__filter__header flex items-center justify-between px-5 py-3">
@@ -19,6 +23,9 @@ const DesktopFilter = () => {
           <span className="text-primary text-sm">Use promo code</span>
         </button>
       </div>
+      <section className="ga__desktop__filter__content px-5 py-3">
+        {activeTab === 3 && <CheckInTab />}
+      </section>
     </section>
   );
 };
---
+++ pages/components/tabs/FlightTab.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable @next/next/no-img-element */
+import PromoIcon from "assets/svgs/promo.svg";
+
+const FlightTab = () => {
+  return (
+    <section className="bg-green">
+     
+    </section>
+  );
+};
+
+export default FlightTab;
