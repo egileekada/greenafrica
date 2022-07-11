b0d383d219355486d273e920ce967ebfc273c9b4
geekdaniels <oladotun.olufemi@yahoo.com>
2022-07-11 11:19:29 +0100

    update seo implementation

--- pages/_app.js
+++ pages/_app.js
@@ -10,57 +10,73 @@ const MyApp = ({ Component, pageProps }) => {
   return (
     <Fragment>
       <Head>
-        <title>Green Africa</title>
-        <meta charSet="UTF-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta
-          name="viewport"
-          content="initial-scale=1.0, minimum-scale=1.0, width=device-width, maximum-scale=1, user-scalable=no"
-        />
-        <meta name="theme-color" content="#000000" />
-        <meta
-          httpEquiv="Cache-Control"
-          content="no-cache, no-store, must-revalidate"
-        />
-        <meta httpEquiv="Pragma" content="no-cache" />
-        <meta httpEquiv="Expires" content="0" />
-        <meta name="author" content="" />
-        <meta name="description" content="" />
-        <meta name="keywords" content="Green Africa" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico"></link>
 
-        <link rel="canonical" href="https://www.greenafrica.com/en-us/" />
-
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="//static.greenafrica.com/assets/imgs/favicons/apple-touch-icon.png?10559"
-        />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon.png"></link>
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="//static.greenafrica.com/assets/imgs/favicons/favicon-32x32.png?10559"
-        />
+          href="/favicon.png"
+        ></link>
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="//static.greenafrica.com/assets/imgs/favicons/favicon-16x16.png?10559"
+          href="/favicon.png"
+        ></link>
+        <link rel="shortcut icon" href="/favicon.png"></link>
+
+        {/* Primary Meta Tags  */}
+        <title>
+          Green Africa — One flight closer to your dreams and destinations.
+        </title>
+        <meta
+          name="title"
+          content="Green Africa — You are one flight closer to your dreams and destinations."
+          key="title"
         />
-        <link
-          rel="mask-icon"
-          href="//static.greenafrica.com/assets/imgs/favicons/safari-pinned-tab.svg?10559"
-          color="#6f2c91"
+        <meta
+          name="description"
+          content="We are a new carrier that offers safe, reliable and affordable air travel to a much broader group of customers."
         />
-        <meta name="msapplication-TileColor" content="#6f2c91" />
-        <meta name="theme-color" content="#ffffff" />
-        <meta name="format-detection" content="telephone=no" />
-        {/* <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/public/images/favicon.png"
-          /> */}
+
+        {/* Open Graph / Facebook  */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://dev-website.gadevenv.com/" />
+        <meta
+          property="og:title"
+          content="Green Africa — One flight closer to your dreams and destinations."
+          key="ogTitle"
+        />
+        <meta
+          property="og:description"
+          content="We are a new carrier that offers safe, reliable and affordable air travel to a much broader group of customers."
+        />
+        <meta
+          property="og:image"
+          content="https://dev-website.gadevenv.com/images/seo_image.jpg"
+        />
+
+        {/* Twitter  */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://dev-website.gadevenv.com/"
+        />
+        <meta
+          property="twitter:title"
+          content="Green Africa — One flight closer to your dreams and destinations."
+          key="twitterTitle"
+        />
+        <meta
+          property="twitter:description"
+          content="We are a new carrier that offers safe, reliable and affordable air travel to a much broader group of customers."
+        />
+        <meta
+          property="twitter:image"
+          content="https://dev-website.gadevenv.com/images/seo_image.jpg"
+        />
       </Head>
       <Fragment>
         <main className="main">
