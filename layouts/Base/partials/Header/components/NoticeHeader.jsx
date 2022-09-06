2f66ced75e1119bc60d143bb999a001ec4404eb7
Folafunmi <folafunmimustapha@gmail.com>
2022-09-06 21:21:17 +0100

    fix: close banner

--- layouts/Base/partials/Header/components/NoticeHeader.jsx
+++ layouts/Base/partials/Header/components/NoticeHeader.jsx
@@ -3,14 +3,24 @@ import { useQuery } from "@tanstack/react-query";
 import { getNotifications } from "../../../../../services";
 import useDeviceSize from "hooks/useWindowSize";
 import CovidIcon from "assets/svgs/bell.svg";
+import { useEffect, useState } from "react";
+
+export const BANNER_SHOW = "greenBannerShow";
 
 const NoticeHeader = () => {
   const { data } = useQuery(["notifications"], getNotifications);
 
   const [width] = useDeviceSize();
+
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    setShow(localStorage?.getItem(BANNER_SHOW) ?? true);
+  }, []);
+
   return (
     <>
-      {data?.data?.item.active == 1 && (
+      {data?.data?.item.active == 1 && show && (
         <header className="w-full mx-auto relative px-8 lg:px-70 py-[18px] bg-primary-light">
           <div className="desktop">
             <div className="flex items-center">
@@ -29,6 +39,15 @@ const NoticeHeader = () => {
                   {data?.data?.item.contents}
                 </marquee>
               )}
+              <p
+                onClick={() => {
+                  setShow(false);
+                  localStorage.setItem(BANNER_SHOW, false);
+                }}
+                className="text-white text-sm underline cursor-pointer ml-2"
+              >
+                Cancel
+              </p>
             </div>
           </div>
         </header>
