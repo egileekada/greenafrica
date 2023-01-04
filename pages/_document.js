5f469aa2b7b453495e7be55cd25474b70ec5ea67
Oladotun Olufemi <olufemidaniels07@gmail.com>
2023-01-04 12:17:27 +0100

    add chatbot to website

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,32 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              function initFreshChat() {
+                      window.fcWidget.init({
+                          token: "3a2bd34c-b92e-4310-bc2d-7f4f1db8b881",
+                          host: "https://greenafrica-350507227206212.freshchat.com"
+                      });
+                  }
+              
+              function initialize(i, t) {
+                  var e;
+                  i.getElementById(t) ?
+                      initFreshChat() : ((e = i.createElement("script")).id = t, e.async = !0,
+                          e.src = "https://greenafrica-350507227206212.freshchat.com/js/widget.js", e.onload = initFreshChat, i.head.appendChild(e))
+              }
+              
+              function initiateCall() {
+                  initialize(document, "Freshchat-js-sdk")
+              }
+              window.addEventListener ? window.addEventListener("load", initiateCall, !1) :
+                  window.attachEvent("load", initiateCall, !1);
+            `,
+            }}
+          />
         </body>
       </Html>
     );
